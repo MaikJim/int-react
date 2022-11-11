29c15a6cb45540fbc5873bc6dc86ee6ce5ed4417
MaikJim <maikoljimenez427@gmail.com>
2022-11-11 18:46:04 -0300

    Feat: Agregando las funciones completar y eliminar ToDos

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { TodoCounter } from './Components/TodoCounter';
-import { TodoSearch } from './Components/TodoSearch';
-import { TodoList } from './Components/TodoList';
-import { TodoItem } from './Components/TodoItem';
-import { CreateTodoButton } from './Components/CreateTodoButton';
+import { AppUI } from './AppUI';
+
 // import './App.css';
 
 const defaultTodos = [
@@ -31,29 +28,32 @@ function App() {
       return todoText.includes(searchText);
     });
   }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text == text);
+    const newTodos =[...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text == text);
+    const newTodos =[...todos];
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos);
+  }
+
+
   return (
-    <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </React.Fragment>
+    <AppUI 
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo= {completeTodo}
+      deleteTodo = {deleteTodo}
+    />
   );
 }
 
